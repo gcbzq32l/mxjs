82e8b84ff642e4a7f0fb76b8e8d3d0da87b459ae
“gcbzq32l” <gcbzq32l@gmail.com>
2022-01-14 20:34:29 +0800

    test2

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ git pull remote main
 git add .
 git commit -m $1
 git push origin main --force
+Username for 'https://github.com': gcbzq32l
+Password for 'https://gcbzq32l@github.com':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
 if [ "$?" = "0" ]
 then
  echo -e "\033[42;34m push to github success! \033[0m"
