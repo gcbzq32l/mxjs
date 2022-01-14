5b2ba26209c4d760c3db104b5612159dcfd51079
“gcbzq32l” <gcbzq32l@gmail.com>
2022-01-14 20:31:47 +0800

    test

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#author Oliver
+#since 2020-09-03 15:24:31
+  
+git remote rm origin
+#replace your git location
+git remote add origin 'https://github.com/gcbzq32l/mxjs.git'
+git pull remote main
+git add .
+git commit -m $1
+git push origin main --force
+if [ "$?" = "0" ]
+then
+ echo -e "\033[42;34m push to github success! \033[0m"
+else
+ echo -e "\033[41;30m push to github fail! \033[0m"
+ exit 1
+fi
